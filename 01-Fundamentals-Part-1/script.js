8b446a61b0aeae58612db050289692fa5d47d5ff
Anil Kunda <anilkunda701@gmail.com>
2025-02-22 20:02:54 +0530

    basic operators

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -82,3 +82,57 @@ console.log(discountCode, typeof discountCode); // object
 let specialOffer;
 // let specialOffer = undefined
 console.log(specialOffer, typeof specialOffer); // undefined
+
+// ------- ** Operators
+
+// --- Operator performs some operation on single or multiple operands (data value) and produces a result.
+
+// ----- • Arithmetic Operators
+
+// 1. Addition (+)
+
+const accountBalance = 1500;
+const depositAmount = 500;
+
+const newBalance = accountBalance + depositAmount;
+console.log(`$${newBalance}`); // $2000
+
+// 2. Subtraction (-)
+
+const withdrawalAmount = 750;
+
+const remainingBalance = newBalance - withdrawalAmount;
+console.log(`$${remainingBalance}`); // $1250
+
+// 3. Multiplication (*)
+
+const principalAmount = 10000;
+const annualInterestRate = 0.05; // Annual interest rate (5%)
+
+const interestEarned = principalAmount * annualInterestRate; // Interest earned in one year
+console.log(`$${interestEarned}`); //  $500
+
+// 4. Division (/)
+
+const loanAmount = 1200;
+const numberOfMonths = 24;
+
+const monthlyPayment = loanAmount / numberOfMonths;
+console.log(`$${monthlyPayment.toFixed(2)}`); // $50.00
+
+// 5. Modulus (%)
+
+const remainder = loanAmount % numberOfMonths; // Any remaining amount
+console.log(`Remainder: ${remainder}`); // Remainder: 0
+
+// 6. Increment (++) and Decrement (--) Operator
+
+let cartQuantity = 1;
+
+// Increment (++)
+cartQuantity++; // Increment quantity by 1
+console.log(`Quantity after adding an item: ${cartQuantity}`); // 2
+
+// Decrement (--)
+cartQuantity--; // Decrement quantity by 1
+console.log(`Quantity after removing an item: ${cartQuantity}`); // 1
