dcbec8420f6cd50f5be07d21dca33522f1c32d76
Anil Kunda <anilkunda701@gmail.com>
2025-02-23 19:51:07 +0530

    javascript functions

--- 02-Fundamentals-Part-2/script.js
+++ 02-Fundamentals-Part-2/script.js
@@ -1,1 +1,66 @@
 'use strict';
+
+// -------** Functions
+
+// --- Function is simply a piece of code that we can reuse over and over again in our code.
+
+// 01. Function Declaration - Function that can be used before it's declared (Hoisting)
+function calcAge(birthyear) {
+  return 2024 - birthyear;
+}
+
+const showAge = calcAge(2002);
+console.log(showAge);
+
+// 02. Function Expression - Essentially a function value store in a variable
+const calcAge2 = function (birthyear) {
+  return 2024 - birthyear;
+};
+
+const showAge2 = calcAge2(2003);
+console.log(showAge2);
+
+// 03. Arrow Function - Great for a quick one-line function. has no this keyword.
+const calcAge3 = (birthyear) => 2024 - birthyear;
+const showAge3 = calcAge(2004);
+console.log(showAge3);
+
+// ----- Create a Fruit Processor Function()
+
+function fruitProcessor(apples, oranges) {
+  console.log(apples, oranges);
+
+  const juice = `Juice with ${apples} apples and ${oranges} oranges`;
+  return juice;
+}
+fruitProcessor(5, 0); // calling / running / invoking function
+
+const appleJuice = fruitProcessor(5, 0);
+console.log(appleJuice);
+
+const appleOrangeJuice = fruitProcessor(2, 4);
+console.log(appleOrangeJuice);
+
+console.log(fruitProcessor(5, 0));
+
+// Parameter : It's a variable in a function definition. it's a placeholder for value that will be provided when the function is called.
+
+// Argumant : It's the actual value that is passed to the function when calling it. These value are assigned to the function's parameters.
+
+// Return : The return statement stops the execution of a function and return a value.
+// The return statemets makes the function reusable you can use the some function in multiple places.
+
+// ----- Function Calling Other Function
+function cutFruitPieces(fruit) {
+  return fruit * 4;
+}
+
+function fruitProcessor2(apple, orange) {
+  const applePieces = cutFruitPieces(apple);
+  const orangePieces = cutFruitPieces(orange);
+
+  const juice = `Juice with ${applePieces} pieces of apple and ${orangePieces} pieces of orange`;
+  return juice;
+}
+
+console.log(fruitProcessor2(2, 4));
