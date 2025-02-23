2935ebe0d33bdeb7e73246f9989c3761a224e1c9
Anil Kunda <anilkunda701@gmail.com>
2025-02-23 20:12:12 +0530

    iteration - the for loop

--- 02-Fundamentals-Part-2/script.js
+++ 02-Fundamentals-Part-2/script.js
@@ -216,3 +216,71 @@ console.log(gagan.age);
 console.log(gagan.age);
 
 console.log(gagan.getSummary());
+
+// ------- ** Loops
+
+// Loops in JavaScript are control structures used to repeatedly execute a block of code as long as a specified condition is true.
+
+// ----- Iteration - The for loop
+
+// console.log("Lifting weights, repetition 1");
+// console.log("Lifting weights, repetition 2");
+// console.log("Lifting weights, repetition 3");
+// console.log("Lifting weights, repetition 4");
+// console.log("Lifting weights, repetition 5");
+
+// for loop keeps running while condition is true.
+
+for (let i = 1; i <= 10; i++) {
+  console.log(`Hello ${i}`);
+}
+
+for (let rep = 1; rep <= 5; rep++) {
+  console.log(`Lifting weights, repetition ${rep}`);
+}
+
+// ----- Looping arrays, breaking and continuing.
+
+const anil = [
+  'Anil',
+  'Kunda',
+  2024 - 2002,
+  'Developer',
+  ['Manoj', 'Gagan', 'Mallesh'],
+  true,
+];
+
+const types = [];
+
+for (let i = 0; i < anil.length; i++) {
+  // Reding from anil array
+  console.log(anil[i], typeof anil[i]);
+
+  // Filling types array
+  types[i] = typeof anil[i];
+  types.push(typeof anil[i]);
+}
+console.log(types);
+
+// Continue and Break - Only String
+for (let i = 0; i < anil.length; i++) {
+  if (typeof anil[i] !== 'string') continue;
+
+  console.log(anil[i], typeof anil[i]);
+}
+
+// Break with Number
+for (let i = 0; i < anil.length; i++) {
+  if (typeof anil[i] === 'number') break;
+
+  console.log(anil[i], typeof anil[i]);
+}
+
+// Exercise
+const yearsArray = [2000, 2002, 2004, 2006];
+const agesArray = [];
+
+for (let i = 0; i < yearsArray.length; i++) {
+  agesArray.push(2025 - yearsArray[i]);
+}
+console.log(agesArray);
