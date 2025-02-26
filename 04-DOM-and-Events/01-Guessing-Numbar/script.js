c28a744b0254e0fcfba848e86a9f0e75d8c7e03a
Anil Kunda <anilkunda701@gmail.com>
2025-02-26 21:48:20 +0530

    manipulating css styles

--- 04-DOM-and-Events/01-Guessing-Numbar/script.js
+++ 04-DOM-and-Events/01-Guessing-Numbar/script.js
@@ -19,10 +19,17 @@ document.querySelector(".check ").addEventListener("click", function () {
   const guess = Number(document.querySelector(".guess").value);
   console.log(guess);
 
+  //   When there is no input
   if (!guess) {
     document.querySelector(".message").textContent = "🚫 No number!";
+
+    // When player wins
   } else if (guess === secretNumber) {
     document.querySelector(".message").textContent = "🎉 Correct Number!";
+    document.querySelector("body").style.backgroundColor = "#60b347";
+    document.querySelector(".number").style.width = "30rem";
+
+    // When guess is too high
   } else if (guess > secretNumber) {
     if (score > 1) {
       document.querySelector(".message").textContent = "📈 Too high!";
@@ -34,6 +41,8 @@ document.querySelector(".check ").addEventListener("click", function () {
       document.querySelector(".score").textContent = score;
       0;
     }
+
+    // When guess is too low
   } else if (guess < secretNumber) {
     if (score > 1) {
       document.querySelector(".message").textContent = "📉 Too low!";
